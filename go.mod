b96e156aca8f34eb88e69dbb2a32345ee6933fb3
f1uff3h <peco@camarad.tech>
2024-10-22 22:06:22 +0300

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module btnsap
 
 go 1.23.0
+
+require (
+	github.com/dennwc/btrfs v0.0.0-20241002142654-12ae127e0bf6 // indirect
+	github.com/dennwc/ioctl v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,43 +1,91 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/dennwc/btrfs"
 )
 
-func usage() {
-	// TODO: add commands dynamically and print usage as expected per command
-	log.Println("Usage: btsnap <command> <arguments>")
-}
+var err error
 
-// func cmd() error {
-// 	argument := os.Args[2]
-//
-// 	if _, err := os.Stat(argument); os.IsNotExist(err) {
-// 		return fmt.Errorf("%s\n\n%s is not a valid filepath", usage(), argument)
-// 	} else if err != nil {
-// 		// TODO: test if filepath is part of BTRFS subvolume
-// 		return fmt.Errorf("unable to check filepath %s: %w", argument, err)
-// 	}
-//
-// 	return nil
-// }
-
+// TODO: add --help flag
 func main() {
-	var err error
-	command := os.Args[1]
-	b := &btsnap{}
+	command, args, err := getCommandArguments()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	switch command {
 	case "shot":
-		err = b.shot()
+		// TODO: maybe move these checks somewhere else?
+		if len(args) < 2 {
+			usageShot()
+			log.Fatal("not enough arguments provided")
+		}
+		if len(args) > 2 {
+			usageShot()
+			log.Fatal()
+		}
+
+		err = shot(args[0], args[1])
 	default:
-		usage()
 		err = fmt.Errorf("unknown command: %s", command)
 	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getCommandArguments() (string, []string, error) {
+	if len(os.Args) < 2 {
+		usage()
+		return "", nil, errors.New("no command provided")
+	}
+	if len(os.Args) < 3 {
+		usage()
+		return "", nil, errors.New("no arguments provided")
+	}
+
+	return os.Args[1], os.Args[2:], nil
+}
+
+func usage() {
+	fmt.Println(`Usage: btsnap <command> <args>
+
+Available commands:
+  shot	take a snapshot of a BTRFS subvolume
+		`)
+}
+
+// FIXME: destSubvolume should be destPath and should allow destPath/someNonExistantDir as everything after / will be created
+func shot(sourceSubvolume, destSubvolume string) error {
+	// NOTE: abstract checks to helper func ?
+	if isSu, err := btrfs.IsSubVolume(sourceSubvolume); err != nil {
+		return fmt.Errorf("unable to check if %s is a BTRFS subvolume: %w", sourceSubvolume, err)
+	} else if !isSu {
+		usageShot()
+		return fmt.Errorf("path %s is not a BTRFS subvolume", sourceSubvolume)
+	}
+	if isSu, err := btrfs.IsSubVolume(destSubvolume); err != nil {
+		return fmt.Errorf("unable to check if %s is a BTRFS subvolume: %w", destSubvolume, err)
+	} else if !isSu {
+		usageShot()
+		return fmt.Errorf("path %s is not a BTRFS subvolume", destSubvolume)
+	}
+
+	// NOTE: provide option for ro?
+	btrfs.SnapshotSubVolume(sourceSubvolume, destSubvolume, false)
+
+	return err
+}
+
+func usageShot() {
+	fmt.Println(`Usage: btsnap shot <source subvolume path> <destination subvolume path>
+
+Takes a snapshot of <source subvolume path> and saves it at <destination subvolume path>
+		`)
 }
